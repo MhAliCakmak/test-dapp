f9db232e41ded5040d854959f742d26d2fea14dc
Mehmet Ali Çakmak <mehmet.cbu63@gmail.com>
2022-03-18 21:29:06 +0300

    Eklentiler eklendi.

--- test-dapp/src/App.js
+++ test-dapp/src/App.js
@@ -1,22 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+// Deploy sonrası verdiği adresi buraya giriyoruz
+const greeterAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 function App() {
+  // local state de kayıtları tutuyoruz.
+  const [greeting, setGreetingValue] = useState()
+
+  // Metamask izni için kullanıyoruz.
+  async function requestAccount() {
+    // cüzdan bağlanmamış ise connect için popup aç
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // smart contract ı çağırma mevcut mesajı okumak için 
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      // ether.js ile provider ı başlat
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      // kontrat başlat
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        // kontratta yer alan greet fonksiyonunu çağır
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        // hata varsa ekrana yaz
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  // smart kontratı çağır ve mesajı güncelle
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      // metamask ile bağlantı için popup açar
+      await requestAccount()
+      // provider ı başlatır
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // cüzdan sahibini tanır
+      const signer = provider.getSigner()
+      // cüzdan sahibi mesajı ayarlayanın mesajını akıllı kontrat için kontrat başlatır
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      // kontrattaki setGreeting mesaj ayarlama fonksiyonuna mesajı gönderir
+      const transaction = await contract.setGreeting(greeting)
+      // transactionın oluşması için bekler
+      await transaction.wait()
+      // transaction oluştuktan sonra mesajı console a basar
+      fetchGreeting()
+    }
+  }
+
+  // React in html kod bloğu 2 buton ve 1 input box oluşturur
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchGreeting}>Selamlamayı Çağır</button>
+        <button onClick={setGreeting}>Selamı Yeniden Ayarla</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Selamlama mesajını değiştir" />
       </header>
     </div>
   );
